8db002ff8e3b96003348fcc3037eacf870665018
Monty Brandenberg <monty@lindenlab.com>
2013-11-03 04:13:20 +0000

    More config work

--- build-cmd.sh
+++ build-cmd.sh
@@ -30,43 +30,43 @@ pushd "$OPENJPEG_SOURCE_DIR"
         "windows")
             load_vsvars
 
-			cmake . -G"Visual Studio 10" -DCMAKE_INSTALL_PREFIX=$stage
+            cmake . -G"Visual Studio 10" -DCMAKE_INSTALL_PREFIX=$stage
             
             build_sln "OPENJPEG.sln" "Release|Win32"
             build_sln "OPENJPEG.sln" "Debug|Win32"
             mkdir -p "$stage/lib/debug"
             mkdir -p "$stage/lib/release"
-            cp bin/Release/openjpeg{.dll,.lib} "$stage/lib/release"
-            cp bin/Debug/openjpeg.dll "$stage/lib/debug/openjpegd.dll"
-            cp bin/Debug/openjpeg.lib "$stage/lib/debug/openjpegd.lib"
-            cp bin/Debug/openjpeg.pdb "$stage/lib/debug/openjpegd.pdb"
-            mkdir -p "$stage/include/openjpeg"
-            cp libopenjpeg/openjpeg.h "$stage/include/openjpeg"
+            cp bin/Release/openjp2{.dll,.lib} "$stage/lib/release"
+            cp bin/Debug/openjp2.dll "$stage/lib/debug/openjp2d.dll"
+            cp bin/Debug/openjp2.lib "$stage/lib/debug/openjp2d.lib"
+            cp bin/Debug/openjp2.pdb "$stage/lib/debug/openjp2d.pdb"
+            mkdir -p "$stage/include/openjp2"
+            cp libopenjp2/openjpeg.h "$stage/include/openjp2"
         ;;
 
         "darwin")
-			cmake . -GXcode -D'CMAKE_OSX_ARCHITECTURES:STRING=i386;ppc' -D'BUILD_SHARED_LIBS:bool=off' -D'BUILD_CODEC:bool=off' -DCMAKE_INSTALL_PREFIX=$stage
-			xcodebuild -configuration Release -target libopenjpeg.a -project openjpeg.xcodeproj
-			xcodebuild -configuration Release -target install -project openjpeg.xcodeproj
+            cmake . -GXcode -D'CMAKE_OSX_ARCHITECTURES:STRING=i386;ppc' -D'BUILD_SHARED_LIBS:bool=off' -D'BUILD_CODEC:bool=off' -DCMAKE_INSTALL_PREFIX=$stage
+            xcodebuild -configuration Release -target libopenjp2.a -project openjpeg.xcodeproj
+            xcodebuild -configuration Release -target install -project openjpeg.xcodeproj
             mkdir -p "$stage/lib/release"
-			cp "$stage/lib/libopenjpeg.a" "$stage/lib/release/libopenjpeg.a"
-            mkdir -p "$stage/include/openjpeg"
-			cp "$stage/include/openjpeg-$OPENJPEG_VERSION/openjpeg.h" "$stage/include/openjpeg"
+            cp "$stage/lib/libopenjp2.a" "$stage/lib/release/libopenjp2.a"
+            mkdir -p "$stage/include/openjp2"
+            cp "$stage/include/openjp2/openjpeg.h" "$stage/include/openjp2"
         ;;
 
         "linux")
-			# Force 4.6
-			export CC=gcc-4.6
-			export CXX=g++-4.6
+            # Force 4.6
+            export CC=gcc-4.6
+            export CXX=g++-4.6
 
-			# Inhibit '--sysroot' nonsense
-			export CPPFLAGS=""
+            # Inhibit '--sysroot' nonsense
+            export CPPFLAGS=""
 
-			cmake -G"Unix Makefiles" \
-				-DCMAKE_INSTALL_PREFIX="$stage" \
-				-DBUILD_SHARED_LIBS:bool=off \
-				-DCMAKE_INSTALL_DEBUG_LIBRARIES=1 .
-			# From 1.4.0:
+            cmake -G"Unix Makefiles" \
+                -DCMAKE_INSTALL_PREFIX="$stage" \
+                -DBUILD_SHARED_LIBS:bool=off \
+                -DCMAKE_INSTALL_DEBUG_LIBRARIES=1 .
+            # From 1.4.0:
             # CFLAGS="-m32" CPPFLAGS="-m32" LDFLAGS="-m32" ./configure --target=i686-linux-gnu --prefix="$stage" --enable-png=no --enable-lcms1=no --enable-lcms2=no --enable-tiff=no
             make
             make install
