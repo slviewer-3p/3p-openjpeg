a5647621c371f9335bd890dfba52b4d231b1e2df
Nat Goodspeed <nat@lindenlab.com>
2016-03-16 11:12:42 -0400

    DRTVWR-418: Apply Callum's build-cmd.sh windows fix to darwin.

--- build-cmd.sh
+++ build-cmd.sh
@@ -56,12 +56,12 @@ pushd "$OPENJPEG_SOURCE_DIR"
             cmake . -GXcode -D'CMAKE_OSX_ARCHITECTURES:STRING=$AUTOBUILD_CONFIGURE_ARCH' \
                     -D'BUILD_SHARED_LIBS:bool=off' -D'BUILD_CODEC:bool=off' \
                     -DCMAKE_INSTALL_PREFIX=$stage -DCMAKE_C_FLAGS="$LL_BUILD"
-            xcodebuild -configuration Release -target openjpeg -project openjpeg.xcodeproj
+            xcodebuild -configuration Release -target openjp2 -project openjpeg.xcodeproj
             xcodebuild -configuration Release -target install -project openjpeg.xcodeproj
             mkdir -p "$stage/lib/release"
             cp "$stage/lib/libopenjp2.a" "$stage/lib/release/libopenjp2.a"
-            mkdir -p "$stage/include/openjp2"
-            cp "$stage/include/openjp2/openjpeg.h" "$stage/include/openjp2"
+            mkdir -p "$stage/include/openjpeg"
+            cp "$stage/include/openjp2/openjpeg.h" "$stage/include/openjpeg"
         ;;
 
         linux*)
